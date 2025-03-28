65eac044f44dbfffe816166adaca20459feaa861
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-03-28 15:49:48 +0100

    feat: add configuration for projector locking

--- 02-async-queries/build.gradle.kts
+++ 02-async-queries/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-integration")
+    implementation("org.springframework.integration:spring-integration-jdbc")
     runtimeOnly("com.h2database:h2")
     implementation("com.opencqrs:framework-spring-boot-starter:1.0.0")
     testImplementation("com.opencqrs:framework-test:1.0.0")
