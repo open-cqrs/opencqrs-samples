d6ab87c30fe678da4e48d0035b5a0ee8b1f7fcf4
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-05-27 15:15:30 +0200

    feat: add javadoc to event classes

--- using-event-streams/src/main/java/com/example/cqrs/domain/api/borrowing/BookCopyLentEvent.java
+++ using-event-streams/src/main/java/com/example/cqrs/domain/api/borrowing/BookCopyLentEvent.java
@@ -3,6 +3,14 @@ package com.example.cqrs.domain.api.borrowing;
 import java.time.Instant;
 import java.util.UUID;
 
+/**
+ *
+ * @see LendBookCommand
+ *
+ * @param id UUID of the lent book copy
+ * @param isbn ISBN of the book, whose copy was lent
+ * @param returnDueAt Return date of the lent book copy
+ */
 public record BookCopyLentEvent(
         UUID id,
         String isbn,
