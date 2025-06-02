a37b4383b46a3b7964e402f5a01943918aa6f42b
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-06-02 13:04:10 +0200

    docs: add JavaDocs to CommandBridge

--- subscription-queries/src/main/java/com/example/cqrs/async/CommandBridge.java
+++ subscription-queries/src/main/java/com/example/cqrs/async/CommandBridge.java
@@ -12,6 +12,10 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This class is wrapper for {@link com.opencqrs.framework.command.CommandRouter} and can be used in its stead.
+ * It provides additional methods for sending commands and then waiting for said commands to be processed asynchronously
+ */
 @Service
 public class CommandBridge {
 
@@ -23,10 +27,27 @@ public class CommandBridge {
         this.channel = channel;
     }
 
+    /**
+     * @see com.opencqrs.framework.command.CommandRouter#send(Command)
+     */
     public <R> R send(Command command) { return commandRouter.send(command); }
 
+    /**
+     * @see com.opencqrs.framework.command.CommandRouter#send(Command, Map)
+     */
     public <R> R send(Command command, Map<String, ?> metadata) { return commandRouter.send(command, metadata); }
 
+    /**
+     *
+     * Sends a command and waits for subsequent events having been processed for a specified group
+     *
+     * @param command the command to be sent to the system
+     * @param group the event-handling group for which to wait for callback from
+     * @return the return value of the command handler responsible for the command
+     * @param <R> the type of the command handlers return value
+     * @throws CqrsFrameworkException
+     * @throws InterruptedException
+     */
     public <R> R sendWaitingForEventsHandled(Command command, String group) throws CqrsFrameworkException, InterruptedException {
 
         var correlationId = UUID.randomUUID().toString();
@@ -60,6 +81,16 @@ public class CommandBridge {
         }
     }
 
+    /**
+     *
+     * Sends a command and waits for subsequent events having been processed for a specified group. Then executes a given runnable immediately after.
+     *
+     * @param command the command to be sent to the system
+     * @param group the event-handling group for which to wait for callback from
+     * @param runnable the {@link Runnable} to be executed after the callback has been received
+     * @return the return value of the command handler responsible for the command
+     * @param <R> the type of the command handlers return value
+     */
     public <R> R sendThenExecute(Command command, String group, Runnable runnable) {
         var correlationId = UUID.randomUUID().toString();
         var unsubscriber = new CompletableFuture<>()
