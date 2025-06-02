7483e91808892159d226d38269f5e3ac173ce1f9
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-06-02 13:10:21 +0200

    docs: add JavaDoc to ReaderRepository

--- subscription-queries/src/main/java/com/example/cqrs/domain/persistence/ReaderRepository.java
+++ subscription-queries/src/main/java/com/example/cqrs/domain/persistence/ReaderRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
+/**
+ * This {@link org.springframework.data.repository.CrudRepository} serves as a simple projection of the relationship between {@link com.example.cqrs.domain.Reader} and {@link com.example.cqrs.domain.Book}
+ */
 public interface ReaderRepository extends CrudRepository<ReaderEntity, UUID> {
 }
